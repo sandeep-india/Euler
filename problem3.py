352044fc5c919570f2b93bddc710844944ae2222
sandeep <deepu730@gmail.com>
2014-05-27 16:23:04 +0530

    update primality

--- problem3.py
+++ problem3.py
@@ -7,17 +7,42 @@ What is the largest prime factor of the number 600851475143 ?
 
 '''
 
+def isprime(n):
+    '''check if integer n is a prime'''
+    # make sure n is a positive integer
+    n = abs(int(n))
+    # 0 and 1 are not primes
+    if n < 2:
+        return False
+    # 2 is the only even prime number
+    if n == 2: 
+        return True    
+    # all other even numbers are not primes
+    if not n & 1: 
+        return False
+    # range starts with 3 and only needs to go up the squareroot of n
+    # for all odd numbers
+    for x in range(3, int(n**0.5)+1, 2):
+        if n % x == 0:
+            return False
+    return True
+
 
 def lprimefac(k):
 	large = 1
-	for i in range(2,((k/2)+1)):
-		if k%i == 0:
-			k= k/i
-			large = i
+	if isprime(k):
+		large = k
+	else:
+		p = k	
+		for i in range(2,(k/2+1)):
+			if p%i == 0:
+				p= p/i
+				if isprime(i):
+					large = i
 	return large
 
 
-print "%d" %(lprimefac(13195))
+print "%d" %(lprimefac(600851475143))
 
 
 '''
--- problem5.py
+++ problem5.py
@@ -7,11 +7,24 @@ What is the smallest positive number that is evenly divisible by all of the numb
 import numpy
 
 def isprime(n):
-	prime = True
-	for i in range(2,n):
-		if n%i == 0:
-			prime = False 
-	return prime
+    '''check if integer n is a prime'''
+    # make sure n is a positive integer
+    n = abs(int(n))
+    # 0 and 1 are not primes
+    if n < 2:
+        return False
+    # 2 is the only even prime number
+    if n == 2: 
+        return True    
+    # all other even numbers are not primes
+    if not n & 1: 
+        return False
+    # range starts with 3 and only needs to go up the squareroot of n
+    # for all odd numbers
+    for x in range(3, int(n**0.5)+1, 2):
+        if n % x == 0:
+            return False
+    return True
 
 
 def lprimefac(k):
