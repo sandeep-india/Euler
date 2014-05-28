cc3e98dd4e1d7ab7dc29cb653b1926e44cbd83f0
sandeep <deepu730@gmail.com>
2014-05-28 23:08:00 +0530

    added solution 12

--- problem5.py
+++ problem5.py
@@ -4,7 +4,7 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 '''
-import numpy
+
 
 def isprime(n):
     '''check if integer n is a prime'''
--- problem9.py
+++ problem9.py
@@ -6,17 +6,12 @@ Find the product abc
 '''
 
 for a in range (1,1000):
-	for b in range(1,1000):
-		for c in range(1,1000):
-			if a+b+c == 1000:
-				if ((a*a)+(b*b) == (c*c)):
-					print a
-					print b
-					print c
-					print (a*b*c)
-					break
-					break
-					break
+	for b in range(a,1000):
+		c = 1000 - a - b
+		if a+b+c == 1000:
+			if ((a*a)+(b*b) == (c*c)):
+				print (a*b*c)
+				break
 
 '''
 
